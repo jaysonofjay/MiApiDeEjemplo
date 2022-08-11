2def033f60517035be2a4c8adc7f09b1ec5af46d
facudok <facudok@gmail.com>
2022-08-11 18:53:35 -0300

    Devolver todo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiPrimeraApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
