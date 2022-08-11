df0739ec587f2547ac9d7dbff3eaf1d53bc9ae61
facudok <facudok@gmail.com>
2022-08-11 18:55:35 -0300

    Errores de Merge

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MiPrimeraApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
